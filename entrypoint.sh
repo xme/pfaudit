a369729e05811f99cc4c6881f2223cc3ec327ebd
Xavier Mertens <xavier@rootshell.be>
2020-12-08 13:38:25 +0100

    Fixed crontab configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if [ -r /.firstboot ]; then
-    echo "*/10 * * * * /pfaudit.py -H $PFAUDIT_HOST -u $PFAUDIT_USER -k $PFAUDIT_SSHKEY -v -j -l $PFAUDIT_LOGFILE >/data/pfaudit.log 2>&1" >/etc/cron.d/pfaudit-cron
+    cat <<__CRON__ >/etc/cron.d/pfaudit-cron
+*/10 * * * * /pfaudit.py -H $PFAUDIT_HOST -u $PFAUDIT_USER -k $PFAUDIT_SSHKEY -v -j -l $PFAUDIT_LOGFILE >>/data/pfaudit.log 2>&1
+
+__CRON__
     chmod 0644 /etc/cron.d/pfaudit-cron
     crontab /etc/cron.d/pfaudit-cron
     touch /var/log/cron.log
